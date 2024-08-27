30fcf20ef016e07d8e0011e8183d126e432e38ec
Abdulholiq13 <abdulholiq571@gmail.com>
2024-08-28 02:34:23 +0500

    added pagination

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -6,17 +6,20 @@ const Products = ({ data, loading }) => {
       {data?.map((product) => {
         return (
           <div
-            className="border border-gray-200 p-4 rounded-xl bg-gray-50"
+            className="group border border-gray-200 p-4 rounded-xl bg-gray-50 cursor-pointer"
             key={product.id}
           >
-            <div className="w-full mb-3 bg-gray-100 rounded-lg border border-gray-300">
+            <div className="w-full mb-3 bg-gray-100 rounded-lg border border-gray-300 relative overflow-hidden">
               <img
-                className="object-contain w-[200px] h-[200px] mx-auto"
+                className="group-hover:scale-[1.03] transition-all duration-200 ease-in-out object-contain w-[200px] h-[200px] mx-auto"
                 src={product.images[0]}
                 alt=""
               />
+              <div className="group-hover:-z-[1] w-full h-full bg-[#00000020] z-30 absolute bottom-0 left-0"></div>
             </div>
-            <p>{product.title}</p>
+            <p className="group-hover:text-blue-400 transition-all duration-200 ease-in-out">
+              {product.title}
+            </p>
           </div>
         );
       })}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,21 @@
 import Products from "@/components/products/Products";
 import { useFetch } from "@/hooks/useFetch";
-import React from "react";
+import { Pagination } from "antd";
+import React, { useState } from "react";
 
 const Home = () => {
-  const { data, loading } = useFetch("/products", { limit: 4 });
+  const [skip, setSkip] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const { data, loading } = useFetch(
+    "/products",
+    { limit, skip: (skip - 1) * limit },
+    [skip, limit]
+  );
+
+  const hadleChange = (current, pageSize) => {
+    setSkip(current);
+    setLimit(pageSize);
+  };
 
   return (
     <div>
@@ -12,6 +24,15 @@ const Home = () => {
       </h2>
 
       <Products data={data?.products} loading={loading} />
+
+      <Pagination
+        className="mt-5"
+        onChange={hadleChange}
+        align="center"
+        defaultCurrent={1}
+        defaultPageSize={limit}
+        total={data?.total}
+      />
     </div>
   );
 };
